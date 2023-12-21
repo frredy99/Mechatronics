ea75752729353a41e09fb50cd2036d68ac9822d6
Ham <frredy99@gmail.com>
2023-12-21 22:08:44 +0900

    bluetooth connection

--- bluetooth_sender.py
+++ bluetooth_sender.py
@@ -1,21 +1,29 @@
 import bluetooth
+import struct
 
-def send_data(data, target_address):
+def send_data(data1, data2, target_address):
     port = 1
 
     try:
         sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
         sock.connect((target_address, port))
-
-        sock.send(data.encode())
-        print(f"Data sent: {data}")
-
+        
+        data1 = struct.pack('f', data1)
+        sock.send(data1)
+        print(f"Data sent: {data1}")
+        
+        data2 = struct.pack('f', data2)
+        sock.send(data2)
+        print(f"Data sent: {data2}")
+        
         sock.close()
+        
     except Exception as e:
         print(f"Error: {e}")
 
 if __name__ == "__main__":
     target_address = "D8:3A:DD:29:9A:A8"
-    data_to_send = "Hello, Raspberry Pi!"
+    data_to_send1 = 6
+    data_to_send2 = 7
 
-    send_data(data_to_send, target_address)
+    send_data(data_to_send1, data_to_send2, target_address)
