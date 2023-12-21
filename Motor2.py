f510985af6b07c6cdcfabcf1c654adbc0c34730b
ham <frredy99@gmail.com>
2023-12-21 09:43:06 +0000

    Motor control with desired time

--- Motor2.py
+++ Motor2.py
@@ -1,18 +1,37 @@
 import pigpio
 import time
+import numpy as np
 
 class MotorControl:
     def __init__(self, servo_pin):
         self.servo_pin = servo_pin
         self.pi = pigpio.pi()
+        self.current_angle = 0
+        self.timestamp = 0.1
+        self.vel = 30       # degree/sec
+        
+        # initializing
+        self.pi.set_servo_pulsewidth(self.servo_pin, 500)
+        time.sleep(1)
+        self.pi.set_servo_pulsewidth(self.servo_pin, 0)
         
     def set_angle(self, angle):
-        MAX_DUTY = 2000
+        MAX_DUTY = 2500
         MIN_DUTY = 500
-        duty_cycle = int((angle / 180.0) * MAX_DUTY + MIN_DUTY)
-        self.pi.set_servo_pulsewidth(self.servo_pin, duty_cycle)
-        time.sleep(10)
+        
+        if angle > self.current_angle:
+            timestamp = self.timestamp
+        else:
+            timestamp = -self.timestamp
+        for i in np.arange(self.current_angle, angle, timestamp):
+            duty_cycle = int((i / 180.0) * (MAX_DUTY-MIN_DUTY) + MIN_DUTY)
+            self.pi.set_servo_pulsewidth(self.servo_pin, duty_cycle)
+            time.sleep(self.timestamp/self.vel)
+            
+            
+        
         self.pi.set_servo_pulsewidth(self.servo_pin, 0)
+        self.current_angle = angle
         
     def cleanup(self):
         self.pi.stop()
@@ -23,15 +42,14 @@ if __name__ == "__main__":
     
     servo1 = MotorControl(18)
     
-    try:
-        servo1.set_angle(90)
-        print("setting angle")
-        time.sleep(1)
-    
-    except KeyboardInterrupt:
-        pass
-
-    finally:
-        servo1.cleanup()
-        print("servo cleaned up")
+    while True:
+        angle = float(input("input angle: "))
+        try:
+            print("setting angle...")
+            servo1.set_angle(angle)
+            print("Done: ", servo1.current_angle)
+        
+        except KeyboardInterrupt:
+            servo1.cleanup()
+            print("servo cleaned up")
 
